9f5c6c306560ec59c93e85d59a7918eb401ea0f9
maxmichalec <mmich8749@gmail.com>
2023-09-10 15:11:16 -0700

    tb progress

--- verilog/dv/cocotb/cocotb_tests.py
+++ verilog/dv/cocotb/cocotb_tests.py
@@ -1,3 +1,4 @@
+import imp
 from hello_world.hello_world import hello_world
 from hello_world_uart.hello_world_uart import hello_world_uart
 from counter_tests.counter_wb.counter_wb import counter_wb
@@ -6,6 +7,12 @@ from counter_tests.counter_la_reset.counter_la_reset import counter_la_reset
 from counter_tests.counter_la_clk.counter_la_clk import counter_la_clk
 from gpio_test.gpio_test import gpio_test
 # from stars_tests.stars_tests import stars_tests
+from project_tests.Absentees.Absentees import Absentees
 from project_tests.DigiDoggs.DigiDoggs import DigiDoggs
 from project_tests.GuitarVillains.GuitarVillains import GuitarVillains
 from project_tests.MatrixMonSTARS.MatrixMonSTARS import MatrixMonSTARS
+from project_tests.SaSS.SaSS import SaSS
+from project_tests.TripleSSS.TripleSSS import TripleSSS
+from project_tests.SyntheSTARS.SyntheSTARS import SyntheSTARS
+from project_tests.SynthSurgeons.SynthSurgeons import SynthSurgeons
+from project_tests.TMNT.TMNT import TMNT
--- verilog/dv/cocotb/project_tests/Absentees/Absentees.py
+++ verilog/dv/cocotb/project_tests/Absentees/Absentees.py
@@ -4,15 +4,37 @@ import cocotb
 caravelEnv : Caravel_env = None
 half_clk_period : int = 0
 
+async def send_pb(val):
+    if val > 0: val += 4
+    # negedge
+    await cocotb.triggers.Timer(half_clk_period * 2, 'ns')
+    caravelEnv.drive_gpio_in(val, 0x1)
+    # negedge
+    await cocotb.triggers.Timer(half_clk_period * 2, 'ns')
+    caravelEnv.drive_gpio_in(val, 0x0)
+    # negedge
+    await cocotb.triggers.Timer(half_clk_period * 2, 'ns')
+
 @cocotb.test() # cocotb test marker
 @report_test # wrapper for configure test reporting files
 async def Absentees(dut):
     cocotb.log.info("[1] Start configuration")
+    global caravelEnv
     caravelEnv = await test_configure(dut, timeout_cycles=2000000)
     cocotb.log.info("[1] Await mgmt_gpio")
+    caravelEnv.drive_gpio_in((5,0), 0x0)
     await caravelEnv.release_csb()
     await caravelEnv.wait_mgmt_gpio(1)
     cocotb.log.info("[1] Finish configuration")
-    half_clk_periodclk_period = caravelEnv.get_clock_period() / 2
-
+    global half_clk_period
+    half_clk_period = caravelEnv.get_clock_period() / 2
     
+    cocotb.log.info(f"[1] T1: Power-on-reset")
+    # reset design
+    caravelEnv.drive_gpio_in((5,0), 0x0)
+
+    await send_pb(0)
+    await cocotb.triggers.ClockCycles(caravelEnv.clk, 1000000)
+
+    await send_pb(1)
+    await cocotb.triggers.ClockCycles(caravelEnv.clk, 500000)
